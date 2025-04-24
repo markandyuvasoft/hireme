062e2253d55fb28fbbaf692e5e6b440147608357
markandyuvasoft <markanddighe3@gmail.com>
2025-04-24 12:41:16 +0530

    socketio

--- index.js
+++ index.js
@@ -1,67 +1,3 @@
-// import express from "express";
-// import dotenv from "dotenv";
-// import { connectDb } from "./config/db.js";
-// import authRouter from "./src/routes/AuthR/authRouter.js";
-// import featuredCategoriesRouter from "./src/routes/feature-categories-R/featured-category-router.js";
-// import featureSubCategoriesRouter from "./src/routes/feature-categories-R/featured-sub-categories-router.js";
-// import blogRouter from "./src/routes/Blog-R/blog-roluter.js";
-// import homeRouter from "./src/routes/Home-R/common-router.js";
-// import serviceRouter from "./src/routes/Service-R/service-router.js";
-// import taskCategoryRouter from "./src/routes/Task-R/task-category/task-category-router.js";
-// import taskSubcategoryRouter from "./src/routes/Task-R/Task-subCategory/task-subcategoryrouter.js";
-// import portfolioRouter from "./src/routes/Portfolio-R/portfolioRouter.js";
-// import bidRouter from "./src/routes/Bid-Task-R/bidTaskRouter.js";
-// import projectRouter from "./src/routes/Project-R/projectRouter.js";
-// import depositRouter from "./src/routes/Deposit-R/depositRouter.js";
-// import mileStoneRouter from "./src/routes/MileStone-R/mileStoneRouter.js";
-// import cors from "cors"; 
-// import morgan from "morgan"; 
-
-
-// dotenv.config();
-
-// const app = express();
-
-// connectDb();
-
-// app.use(express.json());
-// app.use(cors()); 
-// app.use(morgan('dev')); 
-
-
-// app.use("/api/v1", authRouter);
-// app.use("/api/v1", featuredCategoriesRouter);
-// app.use("/api/v1", featureSubCategoriesRouter);
-// app.use("/api/v1", blogRouter);
-// app.use("/api/v1", homeRouter);
-// app.use("/api/v1", serviceRouter);
-// app.use("/api/v1", taskCategoryRouter)
-// app.use("/api/v1", taskSubcategoryRouter)
-// app.use("/api/v1", portfolioRouter)
-// app.use("/api/v1", projectRouter)
-// app.use("/api/v1", depositRouter)
-// app.use("/api/v1", bidRouter)
-// app.use("/api/v1", mileStoneRouter)
-
-
-
-// app.use("/", express.static("public/upload"));
-
-// const port = process.env.PORT || 5000;
-
-// app.listen(port, () => {
-//   console.log(`Server is running on http://0.0.0.0:${port}`);
-// });
-
-// // process.on("SIGINT", () => {
-// //   console.log("Server is shutting down...");
-// //   process.exit();
-// // });
-// -----------------------------------------------------------------------------
-
-
-
-
 import express from "express";
 import dotenv from "dotenv";
 import { connectDb } from "./config/db.js";
@@ -70,7 +6,6 @@ import { Server } from "socket.io";
 import cors from "cors";
 import morgan from "morgan";
 
-// 🛣 All your routes
 import authRouter from "./src/routes/AuthR/authRouter.js";
 import featuredCategoriesRouter from "./src/routes/feature-categories-R/featured-category-router.js";
 import featureSubCategoriesRouter from "./src/routes/feature-categories-R/featured-sub-categories-router.js";
@@ -91,12 +26,10 @@ dotenv.config();
 const app = express();
 connectDb();
 
-// Middlewares
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
 
-// Routes
 app.use("/api/v1", authRouter);
 app.use("/api/v1", featuredCategoriesRouter);
 app.use("/api/v1", featureSubCategoriesRouter);
@@ -112,7 +45,6 @@ app.use("/api/v1", bidRouter);
 app.use("/api/v1", mileStoneRouter);
 app.use("/api/v1", messageRouter);
 
-// Static file serving
 app.use("/", express.static("public/upload"));
 
 const server = http.createServer(app);
@@ -139,6 +71,7 @@ io.on("connection", (socket) => {
       const savedMessage = await newMessage.save();
 
       io.to(receiverId).emit("receive_message", savedMessage);
+      io.to(data.senderId).emit("receive_message", savedMessage);
     } catch (err) {
       console.error("Message save error:", err);
       socket.emit("error_message", { message: "Failed to send message" });
